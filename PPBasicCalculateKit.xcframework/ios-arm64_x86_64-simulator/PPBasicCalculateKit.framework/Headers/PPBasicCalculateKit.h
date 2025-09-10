12ec56ef30d4967f7ad93820d2b9f52088d68df6
tuowei <tuowei@lefu.cc>
2025-09-10 08:34:31 +0800

    feat | 支持模拟器

---
+++ PPBasicCalculateKit.xcframework/ios-arm64_x86_64-simulator/PPBasicCalculateKit.framework/Headers/PPBasicCalculateKit.h
@@ -0,0 +1,27 @@
+//
+//  PPBasicCalculateKit.h
+//  PPBasicCalculateKit
+//
+//  Created by 彭思远 on 2023/3/27.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "abyonCalculate.h"
+#import "BhTwoLegs140.h"
+#import "Bh1TwoLegs140.h"
+#import "Bh4TwoArms140.h"
+#import "Bh3TwoLegs240.h"
+#import "Bh2Body270.h"
+#import "Bh5Body270.h"
+
+
+//! Project version number for PPBasicCalculateKit.
+FOUNDATION_EXPORT double PPBasicCalculateKitVersionNumber;
+
+//! Project version string for PPBluetoothKit.
+FOUNDATION_EXPORT const unsigned char PPBasicCalculateKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPBluetoothKit/PublicHeader.h>
+
+
